283c0459b31d71c2845d651e322a8c1067f242e7
liuyong.wang.o <liuyonng.wang.o@nio.com>
2024-07-03 11:06:41 +0800

    save

--- src/components/layout/header.vue
+++ src/components/layout/header.vue
@@ -2,9 +2,16 @@
 
 <template>
   <div class="layout_header-wrap">
-    <div class="header-left">333</div>
-    <div class="header-right">444</div>
+    <div class="layout_header-left">333</div>
+    <div class="layout_header-right">444</div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.layout_header {
+  &-wrap {
+    display: flex;
+    justify-content: space-between;
+  }
+}
+</style>
