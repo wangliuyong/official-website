42da8c2ebd44d383430af1a3394065b1cf4987a0
liuyong.wang.o <liuyonng.wang.o@nio.com>
2024-07-04 15:27:26 +0800

    使用aliyun 存储图片

--- src/views/home/components/service.vue
+++ src/views/home/components/service.vue
@@ -1,11 +1,5 @@
 <script setup lang="ts">
 import Title from "@/components/title.vue";
-import FBA from "../../../assets/FBA.jpg";
-import air2 from "../../../assets/air2.jpg";
-import owh2 from "../../../assets/owh2.jpg";
-import fcl3 from "../../../assets/fcl3.jpg";
-import package3 from "../../../assets/package.jpg";
-import package4 from "../../../assets/package4.jpg";
 </script>
 
 <template>
@@ -13,14 +7,20 @@ import package4 from "../../../assets/package4.jpg";
   <div class="service_card-list content">
     <div class="service_card-item">
       <div class="img-wrap">
-        <img :src="FBA" alt="" />
+        <img
+          src="https://offical-web.oss-cn-beijing.aliyuncs.com/FBA.jpg?Expires=1720078067&OSSAccessKeyId=TMP.3KfpeFWvkJou96xRT3pgBq5k2jDmee8micix6n9g6Bc6TT8pDcAh2ywBiDALWg33X9fQT7CXSFBwXa2qDTNW65J72LubKi&Signature=T5Ho1CuZIiJanER%2FIitJNjYzabY%3D"
+          alt=""
+        />
       </div>
       <div class="des">中东FBA/FBN专线</div>
     </div>
 
     <div class="service_card-item">
       <div class="img-wrap">
-        <img :src="air2" alt="" />
+        <img
+          src="https://offical-web.oss-cn-beijing.aliyuncs.com/air2.jpg?Expires=1720078121&OSSAccessKeyId=TMP.3KfpeFWvkJou96xRT3pgBq5k2jDmee8micix6n9g6Bc6TT8pDcAh2ywBiDALWg33X9fQT7CXSFBwXa2qDTNW65J72LubKi&Signature=4yebhkk6tPuQUqo2AGyPbLBYub4%3D"
+          alt=""
+        />
       </div>
 
       <div class="des">空运/海运门到门</div>
@@ -28,27 +28,43 @@ import package4 from "../../../assets/package4.jpg";
 
     <div class="service_card-item">
       <div class="img-wrap">
-        <img :src="owh2" height="211" alt="" />
+        <img
+          src="https://offical-web.oss-cn-beijing.aliyuncs.com/owh2.jpg?Expires=1720078198&OSSAccessKeyId=TMP.3KfpeFWvkJou96xRT3pgBq5k2jDmee8micix6n9g6Bc6TT8pDcAh2ywBiDALWg33X9fQT7CXSFBwXa2qDTNW65J72LubKi&Signature=Sn%2FXCZbVqEQdow2%2BkkfQZlW7HQk%3D"
+          height="211"
+          alt=""
+        />
       </div>
       <div class="des">海外仓</div>
     </div>
 
     <div class="service_card-item">
       <div class="img-wrap">
-        <img :src="fcl3" height="211" alt="" />
+        <img
+          src="https://offical-web.oss-cn-beijing.aliyuncs.com/fcl3.jpg?Expires=1720078145&OSSAccessKeyId=TMP.3KfpeFWvkJou96xRT3pgBq5k2jDmee8micix6n9g6Bc6TT8pDcAh2ywBiDALWg33X9fQT7CXSFBwXa2qDTNW65J72LubKi&Signature=12ekc73VHdURwxcze%2F5goO0JBLI%3D"
+          height="211"
+          alt=""
+        />
       </div>
       <div class="des">海运整体柜清仓</div>
     </div>
 
     <div class="service_card-item">
       <div class="img-wrap">
-        <img :src="package3" height="211" alt="" />
+        <img
+          src="https://offical-web.oss-cn-beijing.aliyuncs.com/package.jpg?Expires=1720078221&OSSAccessKeyId=TMP.3KfpeFWvkJou96xRT3pgBq5k2jDmee8micix6n9g6Bc6TT8pDcAh2ywBiDALWg33X9fQT7CXSFBwXa2qDTNW65J72LubKi&Signature=bFC0EozXScIgwzY8TTMcHOX1rpI%3D"
+          height="211"
+          alt=""
+        />
       </div>
       <div class="des">Saber证书</div>
     </div>
     <div class="service_card-item">
       <div class="img-wrap">
-        <img :src="package4" height="211" alt="" />
+        <img
+          src="https://offical-web.oss-cn-beijing.aliyuncs.com/package4.jpg?Expires=1720078251&OSSAccessKeyId=TMP.3KfpeFWvkJou96xRT3pgBq5k2jDmee8micix6n9g6Bc6TT8pDcAh2ywBiDALWg33X9fQT7CXSFBwXa2qDTNW65J72LubKi&Signature=p0%2FyYmn0aeJrZKUBbfP6C%2FgwtF0%3D"
+          height="211"
+          alt=""
+        />
       </div>
       <div class="des">B2C小包</div>
     </div>
